fa6a02bb2bd5db920af48e52974278e632583fb9
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2022-03-12 13:02:20 +0300

    Start
    Sart

---
+++ Table/Cashier.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Table
+{
+    /// <summary>
+    /// Логика взаимодействия для Cashier.xaml
+    /// </summary>
+    public partial class Cashier : Window
+    {
+        private MainWindow main;
+        List<string> list = new List<string>();
+
+        public Cashier()
+        {
+            InitializeComponent();
+        }
+
+        public Cashier(MainWindow main)
+        {
+            this.main = main;
+            InitializeComponent();
+        }
+
+        private void BtnTermAdd1_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            string text;
+            do
+            {
+                int a = random.Next(100);
+                char b = (char)random.Next('A', 'Z');
+                text = b + "" + a;
+                if (!list.Contains(text)) list.Add(text);
+            } while (!list.Contains(text));
+            main.NumberChange.Text += text + "\n";
+        }
+
+        private void BtnTermAdd2_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            string text;
+            do
+            {
+                int a = random.Next(100);
+                char b = (char)random.Next('A', 'Z');
+                text = b + "" + a;
+                if (!list.Contains(text)) list.Add(text);
+            } while (!list.Contains(text));
+            main.NumberChange.Text += text + "\n";
+        }
+
+        private void BtnCashAdd1_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            string text;
+            do
+            {
+                int a = random.Next(100);
+                char b = (char)random.Next('A', 'Z');
+                text = b + "" + a;
+                if (!list.Contains(text)) list.Add(text);
+            } while (!list.Contains(text));
+            main.NumberChange.Text += text + "\n";
+        }
+
+        private void BtnCashAdd2_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            string text;
+            do
+            {
+                int a = random.Next(100);
+                char b = (char)random.Next('A', 'Z');
+                text = b + "" + a;
+                if (!list.Contains(text)) list.Add(text);
+            } while (!list.Contains(text));
+            main.NumberChange.Text += text + "\n";
+        }
+    }
+}
---
+++ Table/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Table
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> list = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Cashier cashier = new Cashier(this);
+            cashier.Show();
+            
+        }
+    }
+}
