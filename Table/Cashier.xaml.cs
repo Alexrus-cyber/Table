9abc99b8a5432170ab90ba9b170755c49a1042fd
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2022-03-15 11:13:09 +0300

    а
    аааа

--- Table/Cashier.xaml.cs
+++ Table/Cashier.xaml.cs
@@ -17,11 +17,13 @@ namespace Table
     /// <summary>
     /// Логика взаимодействия для Cashier.xaml
     /// </summary>
+
     public partial class Cashier : Window
     {
         private MainWindow main;
         List<string> list = new List<string>();
-
+        List<string> list1 = new List<string>();
+        public byte f = 0;
         public Cashier()
         {
             InitializeComponent();
@@ -44,7 +46,8 @@ namespace Table
                 text = b + "" + a;
                 if (!list.Contains(text)) list.Add(text);
             } while (!list.Contains(text));
-            main.NumberChange.Text += text + "\n";
+
+            main.ListView1.Items.Add(text);
         }
 
         private void BtnTermAdd2_Click(object sender, RoutedEventArgs e)
@@ -58,10 +61,10 @@ namespace Table
                 text = b + "" + a;
                 if (!list.Contains(text)) list.Add(text);
             } while (!list.Contains(text));
-            main.NumberChange.Text += text + "\n";
+            main.ListView1.Items.Add(text);
         }
 
-        private void BtnCashAdd1_Click(object sender, RoutedEventArgs e)
+        private void BtnCashAdd1_Click(object sender, RoutedEventArgs e) // Создание рандомных заказов
         {
             Random random = new Random();
             string text;
@@ -72,21 +75,70 @@ namespace Table
                 text = b + "" + a;
                 if (!list.Contains(text)) list.Add(text);
             } while (!list.Contains(text));
-            main.NumberChange.Text += text + "\n";
+            main.ListView1.Items.Add(text);
+            //RadioButton rb = new RadioButton { IsChecked = false, Content = text + "\n" };
+            //top.LetsGo.Children.Add(rb);
         }
 
         private void BtnCashAdd2_Click(object sender, RoutedEventArgs e)
         {
-            Random random = new Random();
-            string text;
-            do
+            //string text;
+            //do
+            //{
+            //    int a = random.Next(100);
+            //    char b = (char)random.Next('A', 'Z');
+            //    text = b + "" + a;
+            //    if (!list.Contains(text)) list.Add(text);
+            //} while (!list.Contains(text));
+            //main.NumberChange.Text += text + "\n";
+            //if ()
+            //{
+            //    list = main.NumberChange.Text.Split('\n').ToList();
+
+            //    list1.Add(list[list.Count - 1]);
+            //    list.RemoveAt(list.Count - 1);
+
+            //     main.NumberChange.Text = string.Join("\n", list.ToArray()); 
+            //    f = 0;
+            //    if (f == 0)
+            //    {
+
+            //       // main.NumberChangeReady.Text = list1[list1.Count - 1].ToString() + "\n";
+            //        main.NumberChangeReady.Text = string.Join("", list1[list1.Count - 1].ToArray());
+
+            //    }
+            //}
+            //if (list.Count > 0)
+            //{
+            //    main.ListView1.Items.RemoveAt(list[list.Count - 1]);
+
+            //}
+            ListViewItem listViewItem = new ListViewItem();
+
+            foreach (var item in list)               // Проверка листа на выделение, удаление выделенных элементов.
             {
-                int a = random.Next(100);
-                char b = (char)random.Next('A', 'Z');
-                text = b + "" + a;
-                if (!list.Contains(text)) list.Add(text);
-            } while (!list.Contains(text));
-            main.NumberChange.Text += text + "\n";
+                if (listViewItem != null)
+                {
+                    listViewItem.IsSelected = listViewItem.IsSelected;
+                    main.ListView2.Items.Add(main.ListView1.SelectedItem);
+                    if (listViewItem.Content == null)
+                    {
+                        main.ListView2.Items.Remove(main.ListView2.SelectedItem);
+                        main.ListView1.Items.Remove(main.ListView1.SelectedItem);
+                    }
+                    main.ListView1.Items.Remove(main.ListView1.SelectedItem);
+
+                }
+            }
+            //main.ListView1.Items.Remove(main.ListView1.SelectedItem);
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
         }
     }
 }
